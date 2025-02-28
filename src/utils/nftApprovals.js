f9661577e5acf7d1f55aea0b6407b14df9f3eddb
memery1446 <memery1446@gmail.com>
2025-02-28 15:44:21 -0500

    721 work.2

--- src/utils/nftApprovals.js
+++ src/utils/nftApprovals.js
@@ -1,7 +1,7 @@
 import { Contract, JsonRpcProvider, ZeroAddress, getAddress } from "ethers";
 import { getProvider } from "./provider";
 import { CONTRACT_ADDRESSES } from "../constants/abis"; // ✅ Correct import
-
+import { NFT_ABI } from "../constants/abis";
 /**
  * Fetch ERC-721 approvals for a given user and token ID.
  * @param {string} userAddress - Wallet address of the owner.
