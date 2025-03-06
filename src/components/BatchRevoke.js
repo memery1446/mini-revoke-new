22397393ce6a4585bdf2c550e861a1628f9727d1
memery1446 <memery1446@gmail.com>
2025-03-05 19:09:48 -0500

    first mixedRevoke success

--- src/components/BatchRevoke.js
+++ src/components/BatchRevoke.js
@@ -110,14 +110,7 @@ const BatchRevoke = () => {
   };
 
   return (
-    <div className="card shadow-sm mb-4">
-      <div className="card-header bg-light d-flex justify-content-between align-items-center">
-        <h5 className="mb-0">🔥 Batch Revoke Approvals</h5>
-      </div>
-      <div className="card-body">
 
-      </div>
-    </div>
   );
 };
 
